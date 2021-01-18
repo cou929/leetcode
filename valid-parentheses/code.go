ef96e6cbc80f53ac1c0e5ed1a5bf0c047e728294
Kosei Moriyama <cou929@gmail.com>
2021-01-18 13:50:31 +0900

    Create code.go

---
+++ valid-parentheses/code.go
@@ -0,0 +1,35 @@
+func isValid(s string) bool {
+    return sol1(s)
+}
+
+// time: O(n), space: O(n)
+func sol1(s string) bool {
+    var stack []rune
+    for _, r := range s {
+        if len(stack) > 0 && stack[len(stack)-1] == r {
+
+        }
+        switch r {
+            case '{', '(', '[':
+            var toStack rune
+            switch r {
+                case '{':
+                toStack = '}'
+                case '(':
+                toStack = ')'
+                case '[':
+                toStack = ']'
+            }
+            stack = append(stack, toStack)
+            case '}', ')', ']':
+            if len(stack) == 0 {
+                return false
+            }
+            if stack[len(stack)-1] != r {
+                return false
+            }
+            stack = stack[0:len(stack)-1]
+        }
+    }
+    return len(stack) == 0
+}
