745fb7072e6d5d8c27768210b5d1297500f16a59
Kosei Moriyama <cou929@gmail.com>
2021-04-11 22:56:45 +0900

    Update code.go

--- sort-list/code.go
+++ sort-list/code.go
@@ -1,4 +1,3 @@
-// time: o(n^2), space: o(1)
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -7,6 +6,67 @@
  * }
  */
 func sortList(head *ListNode) *ListNode {
+    return mergeSort(head)
+}
+
+// time: o(nlogn), space: o(1)
+func mergeSort(head *ListNode) *ListNode {
+    if head == nil {
+        return nil
+    }
+    return ms(head, nil)
+}
+
+func ms(left, right *ListNode) *ListNode {
+    if left == right || left.Next == right {
+        return &ListNode{left.Val, nil}
+    }
+    n := 1
+    mn := 0
+    mid := left
+    cur := left
+    for cur.Next != right {
+        cur = cur.Next
+        n++
+        if n/2 > mn {
+            mid = mid.Next
+            mn++
+        }
+    }
+    a := ms(left, mid)
+    b := ms(mid, right)
+    return merge(a, b)
+}
+
+func merge(a, b *ListNode) *ListNode {
+    head := &ListNode{0, nil}
+    curA, curB, curRes := a, b, head
+    for curA != nil || curB != nil {
+        if curA != nil && curB != nil {
+            if curA.Val < curB.Val {
+                curRes.Next = &ListNode{curA.Val, nil}
+                curRes = curRes.Next
+                curA = curA.Next
+            } else {
+                curRes.Next = &ListNode{curB.Val, nil}
+                curRes = curRes.Next
+                curB = curB.Next                
+            }
+        } else if curA != nil {
+            curRes.Next = &ListNode{curA.Val, nil}
+            curRes = curRes.Next
+            curA = curA.Next
+        } else {
+            curRes.Next = &ListNode{curB.Val, nil}
+            curRes = curRes.Next
+            curB = curB.Next
+        }
+    }
+    return head.Next
+}
+
+// time: o(n^2), space: o(1)
+func liner(head *ListNode) *ListNode {
     left := &ListNode{-100001, nil}
     cur := head
     for cur != nil {
